6d476f4152e6a1fd4ce859e8fd550f35ad7f530b
Argedik <72869669+Argedik@users.noreply.github.com>
2021-08-28 21:26:46 +0300

    asd

--- lib/Views/Menues2/Details/3_mapsWidgets.dart
+++ lib/Views/Menues2/Details/3_mapsWidgets.dart
@@ -1,12 +1,41 @@
+import 'package:argedik/Views/Menues3/gameHomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MapsWidgets extends StatelessWidget {
   const MapsWidgets({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hazırlık"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100.0, left: 10.0, right: 10.0),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => GameHomePage()));
+            // return Navigator.push(
+            //     context,
+            //     MaterialPageRoute(builder: (context) => GameHomePage());
+          },
+          child: Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.yellow,
+            child: Center(
+                child: Text(
+              "Oyuna Başla",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40),
+            )),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/Views/Menues3/gameHomePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameHomePage extends StatelessWidget {
+  const GameHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
